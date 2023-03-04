7797983170241e02c8176b47f23b6ad7e34ffd5b
sippinyotrippin <van.didenko@gmail.com>
2023-03-04 13:34:16 +0300

    linked list realization

---
+++ linked_list_to_str.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+def stringify(linkedlist, result=()):
+    if linkedlist is None or (linkedlist.data is None and linkedlist.next is None):
+        result = list(result)
+        result.append('None')
+        return ' -> '.join(result)
+
+    else:
+        result = list(result)
+        result.append(str(linkedlist.data))
+        if linkedlist.next is not None:
+            return stringify(linkedlist.next, tuple(result))
+        else:
+            return stringify(Node(None), tuple(result))
+
+
+linked_list = Node(None)
+print(stringify(linked_list))
+
+
+
+
