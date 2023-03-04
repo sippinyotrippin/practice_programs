4fc55fbb0f1b4751b847d1d31c53ea56f4683b0e
sippinyotrippin <van.didenko@gmail.com>
2023-03-04 13:59:46 +0300

    remove irrelevant if-statements

--- linked_list_to_str.py
+++ linked_list_to_str.py
@@ -16,5 +16,9 @@ def stringify(linkedlist, result=()):
         return stringify(linkedlist.next, tuple(result))
 
 
-linked_list = Node(None)
-print(stringify(linked_list))
+linked_list1 = Node(None)
+linked_list2 = Node(1, None)
+linked_list3 = Node(1, Node(2, Node(3, Node(4))))
+print(stringify(linked_list1))
+print(stringify(linked_list2))
+print(stringify(linked_list3))
